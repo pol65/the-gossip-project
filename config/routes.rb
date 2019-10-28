896272372f91c4fd31e02d5b58131007f843be41
Polo <paul.poitrenaud@gmail.com>
2019-10-28 14:03:22 +0100

    avancement

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+  get 'team', :to => 'static_pages#team'
+  get 'contact', :to => 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
