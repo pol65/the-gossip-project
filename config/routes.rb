b046684162129b596278d188a25964d31f889b42
ThomasD972 <thomas.delage972@gmail.com>
2019-10-28 15:41:19 +0100

    Ajout de page avec le nom User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'dynamic_pages/welcome'
   root 'static_pages#index'
   get 'team', :to => 'static_pages#team'
   get 'contact', :to => 'static_pages#contact'
+  get 'welcome', :to => 'dynamic_pages#welcome'
+  get 'welcome/:first_name', :to => 'dynamic_pages#welcome'
+  post '/', :to => 'static_pages#index'
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
