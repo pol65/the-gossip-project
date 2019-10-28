2a584b4f87c287ff5fa802ea5bf19b20edfe5502
franklin-saint <zeligbouillot@gmail.com>
2019-10-28 16:56:57 +0100

    ajouts des auteurs en page d'acceuil

--- app/controllers/dynamic_pages_controller.rb
+++ app/controllers/dynamic_pages_controller.rb
@@ -1,4 +1,8 @@
 class DynamicPagesController < ApplicationController
   def welcome
+  puts Gossip.all
+  end
+  def gossip
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'dynamic_pages/welcome'
-  root 'static_pages#index'
+  root 'dynamic_pages#welcome'
+  get 'home', :to => 'static_pages#index'
   get 'team', :to => 'static_pages#team'
   get 'contact', :to => 'static_pages#contact'
   get 'welcome', :to => 'dynamic_pages#welcome'
   get 'welcome/:first_name', :to => 'dynamic_pages#welcome'
   post '/', :to => 'static_pages#index'
- 
+  get 'gossip/:page', :to => 'dynamic_pages#gossip'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
