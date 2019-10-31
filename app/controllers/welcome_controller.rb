abf9a4f4d2bb32174c54c75a7e8681e9ab64c6b4
Polo <paul.poitrenaud@gmail.com>
2019-10-31 10:02:08 +0100

    nettoyage routes

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+    def show
+        @id = params[:id]
+    end
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
   resources :users
   resources :gossips
   resources :cities
-  
-  root 'dynamic_pages#welcome'
-  get 'home', :to => 'static_pages#index'
-  get 'team', :to => 'static_pages#team'
-  get 'contact', :to => 'static_pages#contact'
-  get 'welcome', :to => 'dynamic_pages#welcome'
-  get 'welcome/:first_name', :to => 'dynamic_pages#welcome'
-  post '/', :to => 'static_pages#index'
-  get 'gossip/:page', :to => 'dynamic_pages#gossip'
-  get 'user/:profile', :to => 'dynamic_pages#user'
+  resources :welcome
+  resources :team
+  resources :contact
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
